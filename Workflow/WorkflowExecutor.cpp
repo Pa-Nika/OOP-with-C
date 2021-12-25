791dc3d7a4df97ecdf88b472376e75f5d1c471a1
Pa-Nika <92669295+Pa-Nika@users.noreply.github.com>
2021-12-25 15:29:18 +0700

    Update WorkflowExecutor.cpp

--- Workflow/WorkflowExecutor.cpp
+++ Workflow/WorkflowExecutor.cpp
@@ -81,10 +81,6 @@ void WorkflowExecutor::callParser() {
 }
 
 void WorkflowExecutor::callBlocks(std::list<std::string>& answer_data) {
-//    for (auto& it : order_worker) {
-//        answer_data = it->work(answer_data);
-//    }
-
     Text my_text(answer_data);
     for (auto& it : order_worker) {
         my_text = it->work(my_text);
